238c75f3cd2f32dfbdea74c1bab67a6d96794dcf
InstaGiftCharms <InstaGiftCharms@gmail.com>
2025-03-19 17:35:42 +0800

    Update configParameters.js

--- configParameters.js
+++ configParameters.js
@@ -5,7 +5,7 @@ class ConfigParameters {
         // Example URLs below (replace with your actual image URLs):
         "https://lh3.googleusercontent.com/pw/AP1GczMhUvNEaDbxrcPonTA-Bi2g8Kv9h469BM7QavVE5zEFwbhI2RhFa4Ut8rp0-MogwaYzVThe_5gUJbXcHT96mqQw0TdvNXioPYbZavAJeC5zmubHSShLT3YT48CNaH4-KeA1qyn3BsWRhIKYVVJphX0Xew=w950-h950-s-no-gm?authuser=0",
         "https://lh3.googleusercontent.com/pw/AP1GczOqHOeBqKu_iNl8mKWvRFRsEAXIfQSw1SzKzlbKU0HacDUc8nepvRHzHVXhijx4QVjYOUaimJIccbuZsdakl7N_gqmZ4wZVdLUt3x3qG63LsZwjqplrryTn81cZzwOmoiA0MCmcKAelUI8dt6swXdfFXA=w950-h950-s-no-gm?authuser=0",
-        "https://lh3.googleusercontent.com/pw/AP1GczO9hRBVMHJTw8gyltP51xtDc4wJbpKTynSxILVkee-Wr3lz4zenUg6KudZNBGUhie6tguXorxfeo5LP76p-2YbqwULh-v6wLoKhA5nh8-MI5f-D1BkafQv4gPmDmfkKs0stn5gP88gNmqPaClieuh2z7Q=w1042-h1039-s-no-gm?authuser=0",
+        "https://lh3.googleusercontent.com/pw/AP1GczNj3XyKQpaImyuWobsl6mY5OmwgVXWQL5R-OAmo0o57BmCDeGk_z6DZGoNeL_XkTGaayilZ_VmIScxQk3GN-hu38uDRwSW1ioCiqq4rDM2o4SELgE6_AUxAMsz7fi2cFl69FRFJF-Lj4GfveI9DmZTZ3w=w1042-h1039-s-no-gm?authuser=0",
 	"https://lh3.googleusercontent.com/pw/AP1GczM1V24F4vzVdQuqDhjqT39kb7Q9k1ADHELHA9YG7euWfzuPuLlrtNzBttFbz_Aa_JEeNTHOmelg9jyL01aSflztVKBWXjVlNmQ2hobWXm_m5mhr-Zzl_kZP1zacBVhvWC3NzbM7Bd47YwI9rCziNjID5Q=w950-h950-s-no-gm?authuser=0",
         "https://lh3.googleusercontent.com/pw/AP1GczNoKPXDHc6Qk563FtyV29dE3O3DLi_H18_yB1MF58ijTWsFhLYVh9zvUEwWEpFSSrRXygkjbDI6sm41L-0Im3epBjeVxVgI1CGBMY0D1OD8_RowWan-IKc-mCHSaSQgLSxeBsDp3ceLi_I9xykHRDWcVQ=w950-h950-s-no-gm?authuser=0"
     ];
